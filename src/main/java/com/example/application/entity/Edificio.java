4d899f28181db8f66a487d2fad1b89b1d0d28d69
Gabriel Rezende <gabrielcastrorezende@gmail.com>
2023-05-15 02:04:40 -0300

    criacao de pasta de atividades

--- src/main/java/com/example/application/entity/Edificio.java
+++
@@ -1,9 +0,0 @@
-package com.example.application.entity;
-
-import jakarta.persistence.Entity;
-
-@Entity
-public class Edificio extends AbstractEntity{
-    private String nome;
-    private String endereco;
-}
