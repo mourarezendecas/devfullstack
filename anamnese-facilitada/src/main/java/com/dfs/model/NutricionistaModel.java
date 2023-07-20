52ce54e1f4f36dfbc3fe76d21b992c6e5dae4d05
Gabriel Rezende <gabrielcastrorezende@gmail.com>
2023-07-20 18:25:05 -0300

    Edição da classe nutricionista para ficar conforme o diagrama

--- anamnese-facilitada/src/main/java/com/dfs/model/NutricionistaModel.java
+++ anamnese-facilitada/src/main/java/com/dfs/model/NutricionistaModel.java
@@ -15,7 +15,8 @@ public class NutricionistaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nome;
     private String CRN;
+    private String nome;
+    private String email;
     private String senha;
 }
