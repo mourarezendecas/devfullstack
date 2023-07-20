cdaebe60bc4eed5e4f46b43ebd00069c7edbe892
Gabriel Rezende <gabrielcastrorezende@gmail.com>
2023-07-20 18:36:00 -0300

    Criação da classe PacienteModel

---
+++ anamnese-facilitada/src/main/java/com/dfs/model/PacienteModel.java
@@ -0,0 +1,31 @@
+package com.dfs.model;
+
+import com.dfs.enums.Sexo;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity(name="Pacientes")
+@Table(name="Pacientes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class PacienteModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String nome;
+    Sexo sexo;
+    Date dataNasc;
+    String telefone;
+
+    int horasDeSono;
+    Boolean bebidasAlcoolicas;
+    Boolean tabagismo;
+
+}
